3d403f451d8393efd337193f3ea1869adb682013
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-10-19 18:57:14 +0200

    props name default

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Welcome} from './Welcome'
 function App() {
   return (
     <div className="App">
-      <Welcome name='Alfio' />
+      <Welcome  /> 
     </div>
   );
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -3,6 +3,6 @@ import React from 'react'
 
 export class Welcome extends React.Component {
     render(){
-        return <p> Welcome {this.props.name}  </p> 
+        return <p> Welcome {this.props.name || 'Alfio'}  </p> 
     }
 }
