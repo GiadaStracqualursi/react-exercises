f66032d58550a36a6120a9146237bce5552380e8
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-10-20 18:27:11 +0200

    CounterDisplay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import { Counter } from './Counter';
 import {Welcome} from './Welcome'
 
 function App() {
   return (
     <div className="App">
       <Welcome name={<strong>Alfio</strong>} age= {16} /> 
+      <Counter />
     </div>
   );
 }
