15bc80dcf9ae1de62ad72b58fbe2d8815410e2a6
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-10-20 17:47:02 +0200

    age presente

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,6 +4,7 @@ import {Age} from './Age'
 
 export class Welcome extends React.Component {
     render(){
-        return <p> Welcome {this.props.name || 'Alfio'} {this.props.age > 18 && <Age age= {this.props.age}/>} </p> 
+        return <p> Welcome {this.props.name || 'Alfio'} 
+        {this.props.age && <Age age= {this.props.age}/>} </p> 
     }
 }
