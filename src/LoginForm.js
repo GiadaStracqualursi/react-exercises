be936018fc046d573c72fdbec0da3723bb065def
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-10-22 18:46:00 +0200

    Button Login form

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,31 +1,70 @@
-import { useState } from "react"
+import React from "react";
 
-export function LoginForm(){
-    const [data, setData] = useState({
-        username: '',
-        password: '',
-        remember: false
-    })
+export class LoginForm extends React.Component {
+  state = { username: "", 
+            password: "",
+            remember: false,
+            disabled: true,
+        };
 
-    function handleInputChange(event){
-        const{name, type, value, checked}= event.target
+  handleUsernameInputChange = (event) => {
+    const value = event.target.value;
 
-        setData((data)=>{
-            return{
-            ...data,
-            [name]: type === 'checkbox' ? checked : value
-            }
-        })
+    this.setState({
+      username: value,
+      
+    });
+  };
 
-    }
-
-    return (
+  handleUsernamePasswordChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+        usernamepassword: value,
         
-        <form className="container">
-            <h3>Login function</h3>
-            <input  onChange={handleInputChange} value={data.username} name='username' />
-            <input  onChange={handleInputChange} value={data.password} type ='password' name = 'password' />
-            <input  onChange={handleInputChange} value={data.remember} type = 'checkbox' name = 'remember'/>
-        </form>
-    )
+      });
+  };
+
+  onLogin = (event) => {
+    console.log(event)
+    const verify = event.target.disabled
+    this.setState({
+        disabled : verify
+    })
+    console.log(verify)
+  }
+
+
+  
+
+  render() {
+    return (
+      <div>
+        <h1>My Form</h1>
+        <input
+          name="username"
+          value={this.state.username}
+          onChange={this.handleUsernameInputChange}
+        />
+
+        <input
+          name="password"
+          type= 'password'
+          value={this.state.password}
+          onChange={this.handleUsernamePasswordChange}
+        />
+
+        <input
+          name="remember"
+          type= 'checkbox'
+          value={this.state.remember}
+          onChange={this.handleUsernamePasswordChange}
+        />
+
+        <button disabled={!this.state.username || !this.state.password} 
+        onClick={this.onLogin}>Login
+        </button>
+        
+      </div>
+    );
+  }
 }
