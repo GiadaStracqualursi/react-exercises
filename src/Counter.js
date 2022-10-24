cc00492f44b65f8fb6d34fa8ffff9279ba4699e5
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-10-24 12:32:02 +0200

    Counter.js

---
+++ src/Counter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { CounterDisplay } from './CounterDisplay';
+
+export class Counter extends React.Component{
+    state = {
+		count: 0,
+	}
+
+	componentDidMount() {
+		setInterval(() => {
+			this.setState((state) => {
+				return {
+					count: state.count + 1,
+				}
+			})
+		}, 1000)
+	}
+    
+
+    render (){
+    return <CounterDisplay count={this.state.count}/>}
+}
