19a503b92f3a495a086d826098fb63b43f467cb9
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-10-28 17:39:21 +0200

    GitHubUser

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 
 import './App.css';
-import { ClickCounter } from './ClickCounter';
 
-import { ClickTracker } from './ClickTracker';
-import { Container } from './Container';
-import { CounterFunction } from './CounterFunction';
-import { InteractiveWelcome } from './InteractiveWelcome';
-import { LoginForm } from './LoginForm';
+import { GitHubUser } from './GitHubUser';
 
-import { ToDoList } from './ToDoList';
-
-import { UncontrolledLogin } from './UncontrolledLogin';
-import {Welcome} from './Welcome'
-import { WelcomeFunction } from './WelcomeFunction';
 
 function App() {
 
   return (
     <div className="App">
-      <Welcome name={<strong>Alfio</strong>} age= {16} /> 
-      <ClickCounter/>
-      <ClickTracker/>
-      <InteractiveWelcome/>
-      <LoginForm/>
-      <UncontrolledLogin/>
-      <Container title= {'Ciao Alfio'}/>
-      <WelcomeFunction/>
-      <CounterFunction/>
-      <div>
-      <ToDoList/>
+      <GitHubUser username={'giadastracqualursi'}/>
     
 
       </div>
 
-    </div>
+    
   );
 }
 
