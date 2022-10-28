08127cddb1e7ba48357b76ff62e85ac77d5eedb7
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-10-28 17:55:07 +0200

    GitHubUser

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <GitHubUser username={'giadastracqualursi'}/>
+      <GitHubUser username='giadastracqualursi'/>
     
 
       </div>
