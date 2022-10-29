8348684c9b8aec7893a149bbc92bbb3bc925425c
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-10-29 19:05:51 +0200

    useGitHubUser

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -6,6 +6,18 @@ export function GitHubUser({username}){
 
 const {data, loading, error}= useGitHubUser(username)
 
+function handleGetUserData(){
+    onFetchUser(username)
+}
+
+
+return (<div>
+    <button onClick={handleGetUserData}>Load User data</button>
+{loading && <h1>Loading...</h1>}
+{error && <h1>There has been an error</h1>}
+{data && <h1>{data.username}</h1>}
+</div>)
+
 
 
 }
