ea2e2e0b4bda7be5f67c855ee837c72c4a65481d
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-10-30 21:32:56 +0100

    DisplayLanguage-Function

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,7 +1,8 @@
-import { LanguageContext } from "./LanguageContext"
+import { createContext, useContext } from 'react'
 
-export function DisplayLanguage(){
-    <LanguageContext.Consumer>
-				{(language) => <h1>{language}</h1>}
-			</LanguageContext.Consumer>
+export const LanguageContext = createContext('en')
+
+export function DisplayLanguage() {
+	const language = useContext(LanguageContext)
+	return <h1>{language}</h1>
 }
