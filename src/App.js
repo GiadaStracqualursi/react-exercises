04a2c236e8bfe9ad9a24bdfcd6449b3f51388089
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-11-01 17:37:29 +0100

    React-Router-06

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route } from "react-router-dom";
 import { CounterFunction } from "././Components/CounterFunction";
 import {GitHubUser} from '././Components/GitHubUser';
 import { Link } from "react-router-dom";
+import {ShowGithubUser} from './ShowGithubUser';
 
 
 export function App() {
@@ -14,6 +15,8 @@ export function App() {
         <Route path="/" element={ <WelcomeFunction name='John'/>} />
         <Route path="counter" element= {<CounterFunction/>} />
         <Route path="/users/:username" element= {<GitHubUser/>} />
+              <Route path=":username" element={<ShowGithubUser />} />
+		
         <Route path='*' element={<div><p>Not Found</p> 
         <Link to= '/' element={<p>Go Home</p>} />
         </div>} />
