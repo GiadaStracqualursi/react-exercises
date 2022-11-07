9b9cae4d9247c83e38efaf3376e6a8c61c0587b0
GiadaStracqualursi <giada.stracqualursi@gmail.com>
2022-11-07 18:28:25 +0100

    GitHubUsersSwr

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import "././StileComponents/index.css";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { CounterFunction } from "././Components/CounterFunction";
-import {GitHubUser} from '././Components/GitHubUser';
+
 import { Link } from "react-router-dom";
-import {ShowGithubUser} from './ShowGithubUser';
+
+import { GitHubUserSwr } from "./GitHubUsersSwr";
 
 
 export function App() {
+  
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <WelcomeFunction name='John'/>} />
         <Route path="counter" element= {<CounterFunction/>} />
-        <Route path="/users/:username" element= {<GitHubUser/>} />
-              <Route path=":username" element={<ShowGithubUser />} />
-              <Route index element={<div>Add a user and select it</div>} />
+   
+        <Route index element={<div>Add a user and select it</div>} />
+        <Route path='users'element={<GitHubUserSwr/>}/>
+        
 		
         <Route path='*' element={<div><p>Not Found</p> 
         <Link to= '/' element={<p>Go Home</p>} />
+        
         </div>} />
 
       </Routes>
@@ -29,7 +33,7 @@ export function App() {
       <hr/>
       <Link to= '/counter'>Count</Link>
       <hr/>
-      <Link to= '/users/Giada S'>User</Link>
+      <Link to= '/users' element={<p>Users</p>}/>
       <hr/>
       
     </div>
